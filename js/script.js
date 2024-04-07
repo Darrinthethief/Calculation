493b405d0c2ab1225c24a82dc8982055cdc55b14
Demar English <demarenglish@Demars-MacBook-Pro.local>
2024-04-07 00:25:28 -0400

    Updating the structure of the webpage and interactivity

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const firstNumber = 10;
+const secondNumber = 20;
+
+function add(firstNumber, secondNumber) {
+    console.log(firstNumber + secondNumber)
+}
+
+function subtract(firstNumber, secondNumber) {
+    console.log(firstNumber - secondNumber);
+}
+
+function multiply(firstNumber, secondNumber) {
+    console.log(firstNumber * secondNumber);
+}
+
+function divide(firstNumber, secondNumber) {
+    console.log(firstNumber / secondNumber);
+}
+
+function operator() {
+
+}
