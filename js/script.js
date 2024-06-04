0fa5d2231bc9019f77efa2d5934c8fab8fa39c37
Demar English <demarenglish@Demars-MBP.attlocal.net>
2024-06-04 17:47:56 -0400

    updating features

--- js/script.js
+++ js/script.js
@@ -1,22 +1,66 @@
-const firstNumber = 10;
-const secondNumber = 20;
-
 function add(firstNumber, secondNumber) {
-    console.log(firstNumber + secondNumber)
+    return firstNumber + secondNumber;
 }
 
 function subtract(firstNumber, secondNumber) {
-    console.log(firstNumber - secondNumber);
+    return firstNumber - secondNumber;
 }
 
 function multiply(firstNumber, secondNumber) {
-    console.log(firstNumber * secondNumber);
+    return firstNumber * secondNumber;
 }
 
 function divide(firstNumber, secondNumber) {
-    console.log(firstNumber / secondNumber);
+    if (secondNumber === 0) {
+        return "Error: Division by Zero"
+    }
+    return firstNumber / secondNumber;
 }
 
-function operator() {
+function operator(operand, firstNumber, secondNumber) {
 
+    switch(operand) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+    }
 }
+
+const displayScreen = document.querySelector('.screen-display');
+const clearButton = document.querySelector('#clear');
+const deleteButton = document.querySelector("#delete");
+const percentButton = document.querySelector("#percent");
+const numberButtons = document.querySelectorAll('.numbers');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalButton = document.querySelector('#equal');
+
+let firstNumber = null;
+let secondNumber = null;
+let currentOperator = null;
+let displayValue = '';
+
+function clearDisplay() {
+    firstNumber = null;
+    secondNumber = null;
+    currentOperator = null;
+    displayValue = '';
+    displayScreen.textContent = "0";
+    return;
+}
+
+clearButton.addEventListener('click', clearDisplay);
+
+function populateDisplay() {
+    numberButtons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+           console.log(e.target.textContent);
+        });
+    });
+}
+
+populateDisplay();
